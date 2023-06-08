9361657f9bf75526a3f439efcaed7ce3b0ea6e9b
zeun <real_jieun99@daum.net>
2023-06-09 00:30:13 +0900

    s3,lambda,socket 사용해 이미지 실시간 전송

--- chat.py
+++ chat.py
@@ -1,6 +1,14 @@
 from chat import create_app, socketio
-
+from flask import session, redirect,  request, jsonify
 app = create_app(debug=True)
 
+@app.route('/image', methods=['GET'])
+def image():
+    url = request.args.get('url')
+    session['url']=url
+    socketio.emit('image', {'image_url': session['url']}, broadcast=True, namespace='/test')
+ 
+    return jsonify({'status': 'success'})
+
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app,host='0.0.0.0', debug=True)
--- chat/routes.py
+++ chat/routes.py
@@ -1,10 +1,11 @@
-from flask import session, redirect, url_for, render_template, request, Blueprint
-
+from flask import session, redirect, url_for, render_template, request, Blueprint, jsonify
+from flask_socketio import SocketIO
 from flask_cors import CORS
 
 
 main = Blueprint('main', __name__)
 cors = CORS(main, resources={r"/*": {"origins": "*"}})
+# socketio = SocketIO(main)
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
@@ -23,12 +24,21 @@ def chat():
         return redirect(url_for('.index'))
     return render_template('chat.html', name=name, room=room)
 
+# url = None
+# @main.route('/image', methods=['GET'])
+# def image():
+#     global url
+#     url = request.args.get('url')
+#     session['image_url'] = url
+#     socketio.emit('image', {'image_url': session.get('image_url')}, broadcast=True, namespace='/test')
+#     return jsonify({'status': 'success'})
 
-@main.route('/image', methods=['GET'])
-def image():
-    url = request.args.get('url')
-    print("################ ",url)
-    return "ok"+url;
+@main.route('/dynamic-url', methods=['GET'])
+def dynamic_url():
 
+    return {'url': url}
 
-
+# @socketio.on('connect')
+# def handle_connect():
+#     image_url = url  
+#     emit('update_image', {'image_url': image_url}, broadcast=True)
